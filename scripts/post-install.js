896ba93ade780cb12dc4517d9737f4012bb4191e
Luke Leber <lal65@psu.edu>
2022-08-18 10:08:54 -0400

    Tighten up CI

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -4,11 +4,12 @@ const path = require('path');
 
 const components = fs.readdirSync(path.join('node_modules', '@psu-ooe'));
 components.forEach(component => {
-  fse.mkdirpSync(path.join('components', component));
-  fse.copySync(
+  fse.moveSync(
     path.join('node_modules', '@psu-ooe', component),
     path.join('components', component), {
       overwrite: true
     }
   );
 });
+
+fse.removeSync('node_modules');

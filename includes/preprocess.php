431fc4e6d84e5b8f47102c658f7a06d7809f933d
Luke Leber <lal65@psu.edu>
2022-12-21 09:49:39 -0500

    Github action test

--- includes/preprocess.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-foreach (['html', 'off_canvas_page_wrapper', 'region', 'block', 'menu'] as $element) {
-  require_once(__DIR__ . DIRECTORY_SEPARATOR . 'preprocess' . DIRECTORY_SEPARATOR . $element . '.php');
-}
--- includes/theme_suggestions_alter.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-foreach (['block'] as $element) {
-  require_once(__DIR__ . DIRECTORY_SEPARATOR . 'theme_suggestions_alter' . DIRECTORY_SEPARATOR . $element . '.php');
-}

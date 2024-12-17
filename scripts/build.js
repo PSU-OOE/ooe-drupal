99574dcfbe24c1a6be1b73d167e4fe9e381f2ec7
Luke Leber <lal65@psu.edu>
2024-12-17 13:10:24 -0500

    Tweak build process

--- scripts/build.js
+++ scripts/build.js
@@ -2,6 +2,45 @@ const fs = require('fs');
 const sass = require('sass');
 const uglify = require('uglify-js');
 
+
+let upstream_components_dir = 'upstream-components'
+
+fs.readdirSync(upstream_components_dir).forEach(component => {
+  const upstream_component_dir = upstream_components_dir + '/' + component;
+  const src_dir = upstream_component_dir + '/src';
+
+  const scss_src = src_dir + '/scss/styles.scss';
+  const js_src = src_dir + '/js/scripts.js';
+
+  const dist_dir = upstream_component_dir + '/dist';
+
+  // Compile and minify SCSS.
+  if (fs.existsSync(scss_src)) {
+    fs.mkdirSync(dist_dir, {
+      recursive: true,
+    });
+    const result = sass.compile(scss_src, {
+      style: 'compressed',
+    });
+    fs.writeFileSync(dist_dir + '/styles.css', result.css);
+  }
+
+  // Minify JS.
+  if (fs.existsSync(js_src)) {
+    fs.mkdirSync(dist_dir, {
+      recursive: true
+    });
+    const input_code = fs.readFileSync(js_src).toString('utf-8');
+    const result = uglify.minify(input_code, {
+      compress: {
+        inline: false,
+      },
+    });
+    fs.writeFileSync(dist_dir + '/scripts.js', result.code);
+  }
+});
+
+
 let components_dir = 'components'
 
 fs.readdirSync(components_dir).forEach(component => {
--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,7 +1,7 @@
 const fse = require('fs-extra');
 const path = require('path');
 fse.copySync(
-  path.join('node_modules', '@psu-ooe'),
+  path.join('node_modules', '@psu-ooe', 'components', 'packages'),
   'upstream-components',
   { overwrite: true }
 );
